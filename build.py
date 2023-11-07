d0b55bd87bca1961266231d0cea6f293967d4275
Evgeni Pandurski <epandurski@gmail.com>
2023-11-07 21:07:27 +0200

    Remove annotate param

--- build.py
+++ build.py
@@ -30,7 +30,6 @@ def build():
         compiler_directives={
             "language_level": 3,
         },
-        annotate=False,
     )
     cmd = build_ext(Distribution({"ext_modules": ext_modules}))
     cmd.ensure_finalized()
