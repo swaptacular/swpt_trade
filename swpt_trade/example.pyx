66779955cecf00399698b4763b0fe39e881f75ee
Evgeni Pandurski <epandurski@gmail.com>
2023-11-09 22:36:18 +0200

    Work on example.pyx

--- swpt_trade/example.pyx
+++ swpt_trade/example.pyx
@@ -2,6 +2,7 @@
 
 from libcpp.vector cimport vector
 from libcpp.unordered_map cimport unordered_map
+from libc.stdlib cimport malloc, free
 from cython.operator cimport dereference as deref
 import math
 
@@ -57,6 +58,68 @@ cdef extern from *:
 
     cdef Arc* get_arc(Node* node, size_t index) except +:
         return &node.arcs.at(index)
+
+
+cdef class Digraph:
+    cdef nodemap* debtors
+    cdef nodemap* creditors
+
+    def __cinit__(self):
+        self.debtors = new nodemap()
+        self.creditors = new nodemap()
+        # self._vmap = {ROOT_VERTEX: []}
+
+    cdef void add_supply(self, i64 debtor_id, double amount, i64 creditor_id):
+        cdef Node* debtor_ptr = get_node(self.debtors, debtor_id)
+        if debtor_ptr == NULL:
+            raise RuntimeError("invalid debtor node")
+
+        cdef Node* node_ptr
+        cdef Node* creditor_ptr = get_node(self.creditors, creditor_id)
+        if creditor_ptr == NULL:
+            node_ptr = <Node*>malloc(sizeof(Node))
+            node_ptr.id = creditor_id
+            node_ptr.arcs = vector[Arc]()
+            node_ptr.min_amount = 0.0
+            node_ptr.visited = False
+            creditor_ptr = add_node(self.creditors, node_ptr)
+
+        add_arc(debtor_ptr, Arc(creditor_ptr, amount))
+
+        # assert v is not None
+        # assert v != ROOT_VERTEX
+        # if u in self._vmap:
+        #     self._vmap[u].append(v)
+        # else:
+        #     self._vmap[u] = [v]
+        #     self._vmap[ROOT_VERTEX].append(u)
+
+    cdef void add_demand(self, i64 creditor_id, i64 debtor_id, double amount):
+        pass
+        # assert v is not None
+        # assert v != ROOT_VERTEX
+        # if u in self._vmap:
+        #     self._vmap[u].append(v)
+        # else:
+        #     self._vmap[u] = [v]
+        #     self._vmap[ROOT_VERTEX].append(u)
+
+    def remove_arc(self, u, v):
+        pass
+        # assert u != ROOT_VERTEX
+        # try:
+        #     vlist = self._vmap[u]
+        #     vlist[vlist.index(v)] = None
+        # except (KeyError, ValueError):
+        #     pass
+
+    def _sink_vertex(self, v):
+        pass
+        # if v != ROOT_VERTEX:
+        #     try:
+        #         del self._vmap[v]
+        #     except KeyError:
+        #         pass
 
 
 cpdef double dist((double, double) point1, (double, double) point2):
