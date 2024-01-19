f61b3133affab6d1f16d0870812ba686eebb3bb8
Evgeni Pandurski <epandurski@gmail.com>
2024-01-19 20:17:37 +0200

    Work on models/solver.py

--- swpt_trade/models/solver.py
+++ swpt_trade/models/solver.py
@@ -3,9 +3,32 @@ from sqlalchemy.sql.expression import null, or_, and_
 from swpt_trade.extensions import db
 
 
+class CollectorAccount(db.Model):
+    debtor_id = db.Column(db.BigInteger, primary_key=True)
+    creditor_id = db.Column(db.BigInteger, primary_key=True)
+    creditor_hash = db.Column(db.SmallInteger, nullable=False)
+    account_id = db.Column(db.String, nullable=False)
+    status = db.Column(
+        db.SmallInteger,
+        nullable=False,
+        default=0,
+        comment=(
+            "Collector account status: 0 means active; otherwise inactive."
+        ),
+    )
+
+
 class Turn(db.Model):
     turn_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    phase = db.Column(db.SmallInteger, nullable=False)
+    phase = db.Column(
+        db.SmallInteger,
+        nullable=False,
+        comment=(
+            "Turn's phase: 10) gathering currencies info; 20) Gathering"
+            " buy and sell offers; 30) applying givings and takings."
+            " More phases may be defined in the future."
+        ),
+    )
     phase_deadline = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
 
 
@@ -13,6 +36,8 @@ class CurrencyInfo(db.Model):
     turn_id = db.Column(db.Integer, primary_key=True)
     debtor_uri = db.Column(db.String, primary_key=True)
     debtor_id = db.Column(db.BigInteger, nullable=False)
+    debtor_info_content_type = db.Column(db.String, nullable=False)
+    debtor_info_sha256 = db.Column(db.LargeBinary, nullable=False)
     peg_debtor_uri = db.Column(db.String)
     peg_debtor_id = db.Column(db.BigInteger)
     peg_exchange_rate = db.Column(db.FLOAT)
@@ -41,24 +66,19 @@ class ConfirmedCurrency(db.Model):
     turn_id = db.Column(db.Integer, primary_key=True)
     debtor_id = db.Column(db.BigInteger, primary_key=True)
 
-    # NOTE: The `debtor_uri` column is not be part of the primary key,
-    # but should be included in the primary key index to allow
+    # NOTE: The rest of the columns are not be part of the primary
+    # key, but should be included in the primary key index to allow
     # index-only scans. Because SQLAlchemy does not support this yet
     # (2024-01-19), the migration file should be edited so as not to
     # create a "normal" index, but create a "covering" index instead.
     debtor_uri = db.Column(db.String, nullable=False)
+    debtor_info_content_type = db.Column(db.String, nullable=False)
+    debtor_info_sha256 = db.Column(db.LargeBinary, nullable=False)
     __table_args__ = (
         db.ForeignKeyConstraint(
             ["turn_id"], ["turn.turn_id"], ondelete="CASCADE"
         ),
     )
-
-
-class Collector(db.Model):
-    creditor_id = db.Column(db.BigInteger, primary_key=True)
-    debtor_id = db.Column(db.BigInteger, primary_key=True)
-    creditor_hash = db.Column(db.SmallInteger, nullable=False)
-    account_id = db.Column(db.String, nullable=False)
 
 
 class SellOffer(db.Model):
@@ -73,7 +93,7 @@ class SellOffer(db.Model):
         ),
         db.ForeignKeyConstraint(
             ["collector_id", "debtor_id"],
-            ["collector.creditor_id", "collector.debtor_id"],
+            ["collector_account.creditor_id", "collector_account.debtor_id"],
             ondelete="RESTRICT",
         ),
         db.CheckConstraint(amount > 0),
@@ -106,7 +126,7 @@ class TraderTaking(db.Model):
         ),
         db.ForeignKeyConstraint(
             ["collector_id", "debtor_id"],
-            ["collector.creditor_id", "collector.debtor_id"],
+            ["collector_account.creditor_id", "collector_account.debtor_id"],
             ondelete="RESTRICT",
         ),
         db.CheckConstraint(amount > 0),
@@ -126,12 +146,12 @@ class CollectorGiving(db.Model):
         ),
         db.ForeignKeyConstraint(
             ["from_creditor_id", "debtor_id"],
-            ["collector.creditor_id", "collector.debtor_id"],
+            ["collector_account.creditor_id", "collector_account.debtor_id"],
             ondelete="RESTRICT",
         ),
         db.ForeignKeyConstraint(
             ["to_creditor_id", "debtor_id"],
-            ["collector.creditor_id", "collector.debtor_id"],
+            ["collector_account.creditor_id", "collector_account.debtor_id"],
             ondelete="RESTRICT",
         ),
         db.CheckConstraint(amount > 0),
@@ -151,12 +171,12 @@ class CollectorTaking(db.Model):
         ),
         db.ForeignKeyConstraint(
             ["to_creditor_id", "debtor_id"],
-            ["collector.creditor_id", "collector.debtor_id"],
+            ["collector_account.creditor_id", "collector_account.debtor_id"],
             ondelete="RESTRICT",
         ),
         db.ForeignKeyConstraint(
             ["from_creditor_id", "debtor_id"],
-            ["collector.creditor_id", "collector.debtor_id"],
+            ["collector_account.creditor_id", "collector_account.debtor_id"],
             ondelete="RESTRICT",
         ),
         db.CheckConstraint(amount > 0),
@@ -176,7 +196,7 @@ class TraderGiving(db.Model):
         ),
         db.ForeignKeyConstraint(
             ["collector_id", "debtor_id"],
-            ["collector.creditor_id", "collector.debtor_id"],
+            ["collector_account.creditor_id", "collector_account.debtor_id"],
             ondelete="RESTRICT",
         ),
         db.CheckConstraint(amount > 0),
