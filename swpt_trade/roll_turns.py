c211e5f95a4228226222cce6e12ba068618557a9
Evgeni Pandurski <epandurski@gmail.com>
2024-01-26 17:23:42 +0200

    Work on roll_turns.try_to_advance_turn_to_phase3()

--- swpt_trade/roll_turns.py
+++ swpt_trade/roll_turns.py
@@ -1,21 +1,126 @@
 from typing import TypeVar, Callable
+from sqlalchemy import select
 from swpt_trade.extensions import db
-from swpt_trade.models import Turn, TS0
+from swpt_trade.models import (
+    CollectorAccount,
+    Turn,
+    CurrencyInfo,
+    SellOffer,
+    BuyOffer,
+    TS0,
+)
+from swpt_trade.aggregation import Solver
 
 
 T = TypeVar("T")
 atomic: Callable[[T], T] = db.atomic
 
 
-@atomic
 def try_to_advance_turn_to_phase3(turn_id: int) -> None:
-    # TODO: Add a real implementation.
     turn = (
         Turn.query.filter_by(turn_id=turn_id)
         .with_for_update()
         .one_or_none()
     )
     if turn and turn.phase == 2:
+        solver = Solver(
+            turn.base_debtor_info_locator,
+            turn.base_debtor_id,
+            turn.max_distance_to_base,
+            turn.min_trade_amount,
+        )
+        _register_currencies(solver, turn_id)
+        _register_collector_accounts(solver, turn_id)
+        solver.analyze_currencies()
+
+        _register_sell_offers(solver, turn_id)
+        _register_buy_offers(solver, turn_id)
+        solver.analyze_offers()
+
+        _try_to_write_solver_results(solver, turn_id)
+
+
+def _register_currencies(solver: Solver, turn_id: int) -> None:
+    with db.engines['solver'].connect() as conn:
+        with conn.execution_options(yield_per=50_000).execute(
+                select(
+                    CurrencyInfo.is_confirmed,
+                    CurrencyInfo.debtor_info_locator,
+                    CurrencyInfo.debtor_id,
+                    CurrencyInfo.peg_debtor_info_locator,
+                    CurrencyInfo.peg_debtor_id,
+                    CurrencyInfo.peg_exchange_rate,
+                )
+                .where(CurrencyInfo.turn_id == turn_id)
+        ) as result:
+            for row in result:
+                if row[3] is None or row[4] is None or row[5] is None:
+                    solver.register_currency(row[0], row[1], row[2])
+                else:
+                    solver.register_currency(*row)
+
+
+def _register_collector_accounts(solver: Solver, turn_id: int) -> None:
+    with db.engines['solver'].connect() as conn:
+        with conn.execution_options(yield_per=50_000).execute(
+                select(
+                    CollectorAccount.collector_id,
+                    CollectorAccount.debtor_id,
+                )
+        ) as result:
+            for row in result:
+                solver.register_collector_account(*row)
+
+
+def _register_sell_offers(solver: Solver, turn_id: int) -> None:
+    with db.engines['solver'].connect() as conn:
+        with conn.execution_options(yield_per=50_000).execute(
+                select(
+                    SellOffer.creditor_id,
+                    SellOffer.debtor_id,
+                    SellOffer.amount,
+                    SellOffer.collector_id,
+                )
+                .where(SellOffer.turn_id == turn_id)
+        ) as result:
+            for row in result:
+                solver.register_sell_offer(*row)
+
+
+def _register_buy_offers(solver: Solver, turn_id: int) -> None:
+    with db.engines['solver'].connect() as conn:
+        with conn.execution_options(yield_per=50_000).execute(
+                select(
+                    BuyOffer.creditor_id,
+                    BuyOffer.debtor_id,
+                    BuyOffer.amount,
+                )
+                .where(BuyOffer.turn_id == turn_id)
+        ) as result:
+            for row in result:
+                solver.register_buy_offer(*row)
+
+
+@atomic
+def _try_to_write_solver_results(solver: Solver, turn_id: int) -> None:
+    turn = (
+        Turn.query.filter_by(turn_id=turn_id)
+        .with_for_update()
+        .one_or_none()
+    )
+    if turn and turn.phase == 2:
+        # for solver.takings_iter():
+        #     db.session.execute(
+        #         insert()
+        #     )
+        # _write_takings(solver, turn_id)
+        # _write_collector_transfers(solver, turn_id)
+        # _write_givings(solver, turn_id)
+
+        CurrencyInfo.query.filter_by(turn_id=turn_id).delete()
+        SellOffer.query.filter_by(turn_id=turn_id).delete()
+        BuyOffer.query.filter_by(turn_id=turn_id).delete()
+
         turn.phase = 3
         turn.phase_deadline = None
         turn.collection_started_at = TS0
--- tests/test_procedures.py
+++ tests/test_procedures.py
@@ -1,11 +1,15 @@
 import pytest
 from datetime import timedelta, datetime, timezone
 from swpt_trade import procedures as p
+from swpt_trade import roll_turns
 from swpt_trade.models import (
+    CollectorAccount,
     Turn,
     DebtorInfo,
     ConfirmedDebtor,
     CurrencyInfo,
+    SellOffer,
+    BuyOffer,
     CollectorSending,
     TS0,
 )
@@ -230,3 +234,86 @@ def test_try_to_advance_turn_to_phase4(db_session):
     assert len(all_turns) == 1
     assert all_turns[0].phase == 4
     assert all_turns[0].phase_deadline is None
+
+
+def test_try_to_advance_turn_to_phase3(db_session):
+    turn = Turn(
+        phase=2,
+        phase_deadline=TS0,
+        collection_started_at=TS0,
+        collection_deadline=TS0,
+        base_debtor_info_locator="https://example.com/101",
+        base_debtor_id=101,
+        max_distance_to_base=5,
+        min_trade_amount=5000,
+    )
+    db_session.add(turn)
+    db_session.flush()
+    db_session.commit()
+    turn_id = turn.turn_id
+
+    db_session.add(
+        CurrencyInfo(
+            turn_id=turn_id,
+            debtor_info_locator='https://example.com/101',
+            debtor_id=101,
+            peg_debtor_info_locator=None,
+            peg_debtor_id=None,
+            peg_exchange_rate=None,
+            is_confirmed=True,
+        )
+    )
+    db_session.add(
+        CurrencyInfo(
+            turn_id=turn_id,
+            debtor_info_locator='https://example.com/102',
+            debtor_id=102,
+            peg_debtor_info_locator='https://example.com/101',
+            peg_debtor_id=101,
+            peg_exchange_rate=2.0,
+            is_confirmed=True,
+        )
+    )
+    db_session.add(
+        CollectorAccount(debtor_id=101, collector_id=998, account_id="998")
+    )
+    db_session.add(
+        CollectorAccount(debtor_id=102, collector_id=999, account_id="999")
+    )
+    db_session.add(
+        SellOffer(
+            turn_id=turn_id,
+            creditor_id=1,
+            debtor_id=101,
+            amount=15000,
+            collector_id=998,
+        )
+    )
+    db_session.add(
+        SellOffer(
+            turn_id=turn_id,
+            creditor_id=2,
+            debtor_id=102,
+            amount=10000,
+            collector_id=999,
+        )
+    )
+    db_session.add(
+        BuyOffer(
+            turn_id=turn_id,
+            creditor_id=1,
+            debtor_id=102,
+            amount=20000,
+        )
+    )
+    db_session.add(
+        BuyOffer(
+            turn_id=turn_id,
+            creditor_id=2,
+            debtor_id=101,
+            amount=30000,
+        )
+    )
+    db_session.commit()
+
+    roll_turns.try_to_advance_turn_to_phase3(turn_id)
