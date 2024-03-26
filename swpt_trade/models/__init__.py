c48fd565ab4ae544272c787e5cc63171536cc599
Evgeni Pandurski <epandurski@gmail.com>
2024-03-26 14:01:51 +0200

    Add WorkerTurn model

--- swpt_trade/models/__init__.py
+++ swpt_trade/models/__init__.py
@@ -4,3 +4,4 @@ from .solver import *  # noqa
 from .debtor_info import *  # noqa
 from .trading_policies import *  # noqa
 from .collecting import *  # noqa
+from .worker_turns import *  # noqa
---
+++ swpt_trade/models/worker_turns.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+from sqlalchemy.sql.expression import null, or_, and_
+from swpt_trade.extensions import db
+
+
+class WorkerTurn(db.Model):
+    turn_id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    started_at = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
+    base_debtor_info_locator = db.Column(db.String, nullable=False)
+    base_debtor_id = db.Column(db.BigInteger, nullable=False)
+    max_distance_to_base = db.Column(db.SmallInteger, nullable=False)
+    min_trade_amount = db.Column(db.BigInteger, nullable=False)
+    phase = db.Column(db.SmallInteger, nullable=False)
+    phase_deadline = db.Column(db.TIMESTAMP(timezone=True))
+    collection_started_at = db.Column(db.TIMESTAMP(timezone=True))
+    collection_deadline = db.Column(db.TIMESTAMP(timezone=True))
+    worker_turn_subphase = db.Column(
+        db.SmallInteger,
+        nullable=False,
+        default=0,
+        comment=(
+            "The worker may divide the processing of each phase to one more"
+            " sub-phases. The initial sub-phase is always `0`, and the final"
+            " sub-phase is always `10`. Sequential sub-phases do not need to"
+            " be (and normally will not be) represented by sequential"
+            " numbers. This gives the freedom to add sub-phases if necessary."
+        ),
+    )
+    __table_args__ = (
+        db.CheckConstraint(base_debtor_id != 0),
+        db.CheckConstraint(max_distance_to_base > 0),
+        db.CheckConstraint(min_trade_amount > 0),
+        db.CheckConstraint(and_(phase > 0, phase <= 3)),
+        db.CheckConstraint(or_(phase > 2, phase_deadline != null())),
+        db.CheckConstraint(or_(phase < 2, collection_deadline != null())),
+        db.CheckConstraint(or_(phase < 3, collection_started_at != null())),
+        db.CheckConstraint(
+            and_(worker_turn_subphase >= 0, worker_turn_subphase <= 10)
+        ),
+        db.Index(
+            "idx_worker_turn_subphase",
+            worker_turn_subphase,
+            postgresql_where=worker_turn_subphase < 10,
+        ),
+        {
+            "comment": (
+                'Represents a circular trading round in which a "worker"'
+                ' server participates. "Worker" servers will watch for'
+                " new and changed rows in the solver's `turn` table, and will"
+                ' copy them off.'
+            ),
+        },
+    )
