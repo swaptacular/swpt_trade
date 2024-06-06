73275dc75f3d29f503a4ac9ae6d2ebd69093da4d
Evgeni Pandurski <epandurski@gmail.com>
2024-06-06 13:56:46 +0300

    Add todo

--- swpt_trade/models/worker_accounts.py
+++ swpt_trade/models/worker_accounts.py
@@ -74,6 +74,27 @@ class InterestRateChange(db.Model):
     # edited so as not to create a "normal" index, but create a
     # "covering" index instead.
 
+    # TODO: Consider solving the following hypothetical problem:
+    #
+    # When the interest rate has been changed twice (or more) in a
+    # relatively short period of time (which normally should not
+    # happen), and the `AccountUpdate` SMP message for the second
+    # change has been received, but the `AccountUpdate` message for
+    # the first change has not been received yet, the data in this
+    # table will indicate the wrong overall interest rate, which may
+    # lead to a wrong calculation for the amount that needs to be
+    # transferred.
+    #
+    # One possible way to fix this problem (if it happens to be a
+    # problem in practice) would be to detect when one or more
+    # `AccountUpdate` messages have been missing, by keeping track of
+    # the numbers in the `last_change_seqnum` field of all received
+    # `AccountUpdate` messages. Then, in case is conceivable that some
+    # changes in the interest rate may have gone unnoticed, the
+    # calculation of the amounts that needs to be transferred would be
+    # postponed until all missing `AccountUpdate` messages have been
+    # received.
+
     creditor_id = db.Column(db.BigInteger, primary_key=True)
     debtor_id = db.Column(db.BigInteger, primary_key=True)
     change_ts = db.Column(db.TIMESTAMP(timezone=True), primary_key=True)
