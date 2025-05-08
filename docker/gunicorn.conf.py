56386eb6c8560deb57d8d6ba6290e17e1f2e9d4f
Evgeni Pandurski <epandurski@gmail.com>
2025-05-08 13:36:41 +0300

    Activate accesslog only if APP_LOG_LEVEL is "debug"

--- docker/gunicorn.conf.py
+++ docker/gunicorn.conf.py
@@ -1,7 +1,8 @@
 import os
 
-accesslog = '-'
+accesslog = "-" if os.getenv("APP_LOG_LEVEL") == "debug" else None
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s %(D)s "%({X-Logging-Context}o)s" "%(f)s" "%(a)s"'
+disable_redirect_access_to_syslog = True
 
 for k, v in os.environ.items():
     if k.startswith("GUNICORN_"):
